e329b352ae44f6a115ea90cc43d2108302d84952
Piotr Sokołowski <dev@sokoloowski.pl>
2021-10-27 12:07:41 +0200

    ECS fix

--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use App\Repository\OfferRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
 
