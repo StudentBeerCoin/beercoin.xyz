f6948354e4819f190e5bf12fdcd31754c425cb5a
Piotr Sokołowski <dev@sokoloowski.pl>
2021-10-29 19:52:46 +0200

    Extended tests

--- tests/Entity/BeerTest.php
+++ tests/Entity/BeerTest.php
@@ -12,6 +12,27 @@ class BeerTest extends TestCase
     public function testEntityGettersSetters(): void
     {
         $beer = new Beer();
+
+        // Check defaults
         self::assertNotNull($beer->getId());
+        self::assertSame(500, $beer->getVolume());
+        self::assertSame(4.5, $beer->getAlcohol());
+        self::assertTrue($beer->isCan());
+        self::assertFalse($beer->isBottle());
+
+        // Set custom beer parameters
+        $beer->setBrand('t_brand');
+        $beer->setName('t_name');
+        $beer->setVolume(450);
+        $beer->setAlcohol(6);
+        $beer->setPacking(Beer::BOTTLE);
+
+        // Check if beer is updated
+        self::assertSame('t_brand', $beer->getBrand());
+        self::assertSame('t_name', $beer->getName());
+        self::assertSame(450, $beer->getVolume());
+        self::assertSame(6.0, $beer->getAlcohol());
+        self::assertTrue($beer->isBottle());
+        self::assertFalse($beer->isCan());
     }
 }
