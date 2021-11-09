68b99e5313af193adae9bd22d6aca1bb923d3893
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-11-09 17:45:44 +0100

    First endpoint

---
+++ src/Controller/ApiBeerController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Beer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+
+/**
+ * @OA\Tag(name="Beer")
+ */
+class ApiBeerController extends AbstractController
+{
+    /**
+     * @Route("/api/beer/{beer}/details", name="beer_details", methods={"GET"})
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns specified beer details",
+     *     @OA\JsonContent(
+     *        type="object",
+     *        @OA\Property(property="id", type="string"),
+     *        @OA\Property(property="brand", type="string"),
+     *        @OA\Property(property="name", type="string"),
+     *        @OA\Property(property="volume", type="number"),
+     *        @OA\Property(property="alcohol", type="number"),
+     *        @OA\Property(property="packing", type="string"),
+     *     ),
+     * )
+     */
+    public function beerDetails(Beer $beer): Response
+    {
+        return new JsonResponse($beer->__toArray());
+    }
+}
