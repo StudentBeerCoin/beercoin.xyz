de751acf6a76fc1ce8d9aa76451b35b004a06fd1
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-11-10 14:18:50 +0100

    Test API beer endpoints

---
+++ tests/Controller/ApiBeerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiBeerTest extends WebTestCase
+{
+    public function testBeerDetails(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/beer/00000000-0000-0000-0000-000000000001/details');
+
+        self::assertResponseIsSuccessful();
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+//        self::assertSelectorTextContains('h1', 'Hello World');
+    }
+}
