bb2c29604bea1a9f445c027bd2fb6bfe6b0458bb
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-11-17 01:12:56 +0100

    Redoc API docs

---
+++ src/Controller/ApiRedocController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiRedocController extends AbstractController
+{
+    /**
+     * @Route("/api/redoc", name="api_redoc")
+     */
+    public function index(): Response
+    {
+        return $this->render('api_redoc/index.html.twig');
+    }
+}
