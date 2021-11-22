e50885c94d514b64dc0bd3dbdf4c3165f12ab796
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-11-23 00:51:22 +0100

    Testing history endpoints

--- tests/Controller/ApiHistoryTest.php
+++ tests/Controller/ApiHistoryTest.php
@@ -16,4 +16,27 @@ class ApiHistoryTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
     }
+
+    public function testHistoryDetailsNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/history/t_unknown/details');
+
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertSame([
+            'message' => 'Transaction t_unknown not found',
+        ], json_decode($client->getResponse()->getContent(), true));
+        self::assertSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testListingAllTransactionsHistory(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/history/transactions');
+
+        self::assertResponseIsSuccessful();
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertCount(1, json_decode($client->getResponse()->getContent(), true));
+    }
 }
