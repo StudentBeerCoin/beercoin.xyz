b82359185b136a35e097ee271edaf37505aec3f0
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-11-22 23:12:41 +0100

    Additional tests for beer API

--- tests/Controller/ApiBeerTest.php
+++ tests/Controller/ApiBeerTest.php
@@ -18,6 +18,19 @@ class ApiBeerTest extends WebTestCase
         self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
     }
 
+    public function testBeerDetailsNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/beer/t_unknown/details');
+
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertSame([
+            'message' => 'Beer t_unknown not found',
+        ], json_decode($client->getResponse()->getContent(), true));
+        self::assertSame(404, $client->getResponse()->getStatusCode());
+    }
+
     public function testAddingNewBeer(): void
     {
         $client = static::createClient();
@@ -39,8 +52,32 @@ class ApiBeerTest extends WebTestCase
         self::assertSame(204, $client->getResponse()->getStatusCode());
 
         $client->request('GET', '/api/beer/beers');
-        assert(is_string($client->getResponse()->getContent()));
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertCount(1, json_decode($client->getResponse()->getContent(), true));
         // FIXME: Disabled assertion - saving new beers to database is not implemented yet
         // self::assertCount(2, json_decode($client->getResponse()->getContent(), true));
+    }
+
+    public function testAddingNewBeerWithIncorrectRequest(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/beer/beers');
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertCount(1, json_decode($client->getResponse()->getContent(), true));
+
+        $beer = [
+            'brand' => 'Test Brand',
+            'name' => 'Test Beer',
+        ];
+        $beerJson = json_encode($beer);
+        self::assertIsString($beerJson);
+        $client->request('POST', '/api/beer/add', [], [], [], $beerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'Missing following params: volume, alcohol, packing',
+        ], json_decode($client->getResponse()->getContent(), true));
     }
 }
