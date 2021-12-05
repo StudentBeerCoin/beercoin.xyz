2f0efdb737348b822bcffa06498a391a6f2185e9
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-05 16:46:36 +0100

    Additional tests

--- tests/Controller/ApiOfferTest.php
+++ tests/Controller/ApiOfferTest.php
@@ -16,4 +16,57 @@ class ApiOfferTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
     }
+
+    public function testOfferDetailsNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/offer/t_unknown/details');
+
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        self::assertSame([
+            'message' => 'Offer t_unknown not found',
+        ], json_decode($client->getResponse()->getContent(), true));
+        self::assertSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testListingOffersToBuy(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/offer/buy/offers');
+
+        self::assertResponseIsSuccessful();
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        $data = json_decode($client->getResponse()->getContent(), true);
+        foreach ($data as $offer) {
+            self::assertSame('buy', $offer['type']);
+        }
+    }
+
+    public function testListingOffers(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/offer/offers');
+
+        self::assertResponseIsSuccessful();
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        $data = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount(2, $data);
+    }
+
+    public function testListingOffersForSale(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/offer/sell/offers');
+
+        self::assertResponseIsSuccessful();
+        self::assertSame('application/json', $client->getResponse()->headers->get('content-type'));
+        self::assertIsString($client->getResponse()->getContent());
+        $data = json_decode($client->getResponse()->getContent(), true);
+        foreach ($data as $offer) {
+            self::assertSame('sell', $offer['type']);
+        }
+    }
 }
