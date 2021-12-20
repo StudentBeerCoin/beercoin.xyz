bac96fc2f5c73ffb8c1787bfae723b5d2cc8a0ce
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-20 22:42:11 +0100

    Testing adding offers

--- tests/Controller/ApiOfferTest.php
+++ tests/Controller/ApiOfferTest.php
@@ -70,6 +70,217 @@ class ApiOfferTest extends WebTestCase
         }
     }
 
+    public function testAddingNewOffer(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => '00000000-0000-0000-0000-000000000001',
+            'beer' => '00000000-0000-0000-0000-000000000001',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [
+                'x' => 1.234,
+                'y' => 4.321,
+            ],
+            'type' => 'buy',
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(204, $client->getResponse()->getStatusCode());
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount + 1, $response);
+    }
+
+    public function testAddingNewOfferWithIncorrectRequest(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => '00000000-0000-0000-0000-000000000001',
+            'beer' => '00000000-0000-0000-0000-000000000001',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [
+                'x' => 1.234,
+                'y' => 4.321,
+            ],
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'Missing following params: type',
+        ], $response);
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount, $response);
+    }
+
+    public function testAddingNewOfferWithNotExistingOwner(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => 't_unknown',
+            'beer' => '00000000-0000-0000-0000-000000000001',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [
+                'x' => 1.234,
+                'y' => 4.321,
+            ],
+            'type' => 'buy',
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'User t_unknown not found',
+        ], $response);
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount, $response);
+    }
+
+    public function testAddingNewOfferWithNotExistingBeer(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => '00000000-0000-0000-0000-000000000001',
+            'beer' => 't_unknown',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [
+                'x' => 1.234,
+                'y' => 4.321,
+            ],
+            'type' => 'buy',
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'Beer t_unknown not found',
+        ], $response);
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount, $response);
+    }
+
+    public function testAddingNewOfferWithIncorrectLocation(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => '00000000-0000-0000-0000-000000000001',
+            'beer' => '00000000-0000-0000-0000-000000000001',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [1.234, 4.321],
+            'type' => 'buy',
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'Missing following params in location: x, y',
+        ], $response);
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount, $response);
+    }
+
+    public function testAddingNewOfferWithIncorrectType(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $offerCount = count(json_decode($client->getResponse()->getContent(), true));
+
+        $offer = [
+            'owner' => '00000000-0000-0000-0000-000000000001',
+            'beer' => '00000000-0000-0000-0000-000000000001',
+            'amount' => 123,
+            'price' => 3.21,
+            'location' => [
+                'x' => 1.234,
+                'y' => 4.321,
+            ],
+            'type' => 'rent',
+        ];
+        $offerJson = json_encode($offer);
+        self::assertIsString($offerJson);
+        $client->request('POST', '/api/offer/add', [], [], [], $offerJson);
+        self::assertSame(400, $client->getResponse()->getStatusCode());
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        self::assertSame([
+            'message' => 'Incorrect request',
+            'details' => 'Incorrect packing type - allowed values: buy, sell',
+        ], $response);
+
+        $client->request('GET', '/api/offer/offers');
+        self::assertIsString($client->getResponse()->getContent());
+        $response = json_decode($client->getResponse()->getContent(), true);
+        self::assertCount($offerCount, $response);
+    }
+
     public function testUpdatingOfferData(): void
     {
         $client = static::createClient();
